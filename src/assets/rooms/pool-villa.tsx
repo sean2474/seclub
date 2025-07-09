55ff0907cd60171874d5d1ec3108547f1d3b9390
sean2474 <47488868+sean2474@users.noreply.github.com>
2025-07-10 03:45:40 +0900

    eslint

--- src/assets/rooms/pool-villa.tsx
+++ src/assets/rooms/pool-villa.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { ImageSlider } from "@/components/ui/image-slider";
 import { roomData } from "@/const/room-data";
-import { BedDouble, Car, Clock, Dog, User2, Users } from "lucide-react";
+import { BedDouble, Car, Clock, Dog, Users } from "lucide-react";
 import Image from "next/image"
 
 export const PoolVilla = () => {
@@ -27,18 +27,13 @@ export const PoolVilla = () => {
         </div>
       </section>
       <main className="mx-auto">
-        <section className="space-y-8 pt-16 center relative mt-[100svh] bg-background">
+        <section className="space-y-8 pt-16 center relative mt-[95svh] bg-background">
           <div className="text-end space-y-4 flex justify-between items-center max-w-6xl p-5">
             <h2 className="text-5xl font-serif">Overview</h2>
             <p className="text-xl">각 독채별로 <span className="text-green-900 font-semibold">프라이빗 가든</span>과 <span className="text-green-900 font-semibold">풀·스파</span>가 완비되어 있어 외부 방해 없이 온전한 휴식을 취할 수 있습니다. 3동과 6동은 직영으로 운영되어 더욱 <span className="text-green-900 font-semibold">깐깐한 </span>관리가 이루어지며, 평일과 주말 모두 <span className="text-green-900 font-semibold">장기 숙박</span>이 가능합니다.</p>
           </div>
-          {/* <div className="absolute z-10 -bottom-1/8 left-0 w-full h-1/4 bg-gradient-to-b from-background to-beige pointer-events-none" /> */}
         </section>
         <section className="bg-background center w-full relative pb-28">
-          {/* <div className="center flex-col">
-            <h3 className="text-5xl font-serif">Private Pool</h3>
-            <h4 className="text-2xl mt-2">SECLUB</h4>
-          </div> */}
           <div className="absolute h-[500px] bottom-0 w-full bg-green-900" />
           <div className="w-full max-w-6xl p-5">
             <div className="relative w-full h-[700px]">
@@ -49,12 +44,6 @@ export const PoolVilla = () => {
                 className="w-full h-auto object-cover"
               />
             </div>
-            {/* <div className="mt-4 ml-3 flex flex-col gap-0.5">
-              <div><strong>크기:</strong> 4.5 × 2.5 × 0.9 m (바닷물 사용)</div>
-              <div><strong>이용 기간:</strong> 7월 ~ 8월 (2024.06.14 ~ 09.08)</div>
-              <div><strong>수위:</strong> 0.75 m 이하 (체크인 시 물 교체)</div>
-              <div><strong>안전 수칙:</strong> 어린이는 안전 장비 착용, 다이빙·점프·뜀박질 금지</div>
-            </div> */}
           </div>
         </section>
         <section className="bg-beige center py-32 grid grid-cols-2 relative text-black">
@@ -122,6 +111,15 @@ export const PoolVilla = () => {
               </h2>
               <ol className="border border-gray-400/50 p-5 mt-5 space-y-4 list-decimal pl-10">
                 <li className="space-y-2">
+                  <div className="font-medium">수영장 안내</div>
+                  <ul className="space-y-1">
+                    <li> - 수영장의 크기는 가로 4.5m, 세로 2.5m, 깊이 0.9m이며 바닷물을 사용합니다.</li>
+                    <li> - 이용 기간은 7월과 8월이며, 시즌은 2024년 6월 14일부터 9월 8일까지입니다.</li>
+                    <li> - 수위는 0.75m 이하로 유지되며, 체크인 시 물을 교체합니다.</li>
+                    <li> - 어린이는 반드시 구명조끼 등 안전 장비를 착용하고 보호자와 함께 이용해야 하며, 다이빙·점프·뜀박질 등 위험 행위는 금지됩니다.</li>
+                  </ul>
+                </li>
+                <li className="space-y-2">
                   <div className="font-medium">추가 요금 안내</div>
                   <ul className="space-y-1">
                     <li> - 최대 인원(4인)을 초과하여 투숙 시 1인당 20,000원의 추가 요금이 부과됩니다.</li>
@@ -144,7 +142,6 @@ export const PoolVilla = () => {
                     <li> - 정부의 ‘일회용품 사용 자제’ 시책에 따라 치약·칫솔 등 욕실용품은 제공되지 않으니 개인 준비를 부탁드립니다.</li>
                     <li> - 스파 설비 고장 위험으로 인해 입욕제 사용은 불가합니다.</li>
                     <li> - 전기 온수 탱크 특성상, 연속 2회 사용 후에는 최소 4시간의 재충전 시간이 필요합니다.</li>
-                    <li> - 수영장 이용 시 어린이는 반드시 구명조끼 등 안전 장구를 착용하고 보호자와 함께 이용해 주시기 바라며, 다이빙·점프·뜀박질 등 위험 행위는 절대 금지됩니다.</li>
                   </ul>
                 </li>
               </ol>
