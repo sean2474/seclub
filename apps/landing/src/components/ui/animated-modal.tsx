9da2415dca12b7d377c49241f90bbccfb6aac2f4
sean2474 <47488868+sean2474@users.noreply.github.com>
2025-07-25 01:49:44 +0900

    camping site template done

--- apps/landing/src/components/ui/animated-modal.tsx
+++ apps/landing/src/components/ui/animated-modal.tsx
@@ -42,18 +42,21 @@ export function Modal({ children }: { children: ReactNode }) {
 export const ModalTrigger = ({
   children,
   className,
+  onClick,
 }: {
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
 }) => {
   const { setOpen } = useModal();
   return (
     <button
       className={cn(
-        "px-4 py-2 rounded-md text-black dark:text-white text-center relative overflow-hidden",
+        "",
         className
       )}
-      onClick={() => setOpen(true)}
+      onClick={onClick}
+      onClickCapture={() => setOpen(true)}
     >
       {children}
     </button>
