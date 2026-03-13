187a713f620f69ea6496ba72e1682e9f6ad54836
sean2474 <47488868+sean2474@users.noreply.github.com>
2026-03-12 23:56:57 -0400

    event tag detail changed

--- apps/landing/src/assets/room-template.tsx
+++ apps/landing/src/assets/room-template.tsx
@@ -76,7 +76,7 @@ export const RoomTemplate = ({ roomInfo }: { roomInfo: RoomInfo }) => {
           <RoomFeatures features={roomInfo.features} />
           <ScrollReveal side="left" className="center mt-2 md:mt-10">
             <LinkEventTracker 
-              eventName="reservation_room_detail" 
+              eventName={`reservation_room_detail_${roomInfo.slug}`}
               location={`room_${roomInfo.slug}_reservation_btn`}
               href="https://m.thankqcamping.com/resv/view.hbb?cseq=1537&path=RP" 
               target="_blank"
